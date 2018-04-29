bd0b279d673060be98d8d63766336a5ceb41977c
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2018-04-28 22:17:23 -0400

    contact form

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
 // MAP
-var mykey = config.MY_KEY;
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-}
+// var mykey = config.MY_KEY;
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+// }
